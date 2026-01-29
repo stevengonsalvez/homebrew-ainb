1df646e8174de13066ac7fc84ba4fbec53177fcc
steven gonsalvez <steven.gonsalvez@gmail.com>
2026-01-29 00:41:12 +0000

    chore: update to v0.5.2-beta1 and add ainb symlink

--- Formula/ainb.rb
+++ Formula/ainb.rb
@@ -2,24 +2,25 @@ class Ainb < Formula
   desc "Terminal-based development environment manager for Claude Code agents"
   homepage "https://github.com/stevengonsalvez/agents-in-a-box"
   license "MIT"
-  version "0.5.0"
+  version "0.5.2-beta1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/stevengonsalvez/agents-in-a-box/releases/download/v0.5.0/agents-box-0.5.0-aarch64-apple-darwin.tar.gz"
-      sha256 "cde8797e5ac2d3c0f4e9e8d750eeedb9f927fa0084f0b90bf3b73a37f7d4f718"
+      url "https://github.com/stevengonsalvez/agents-in-a-box/releases/download/v0.5.2-beta1/agents-box-0.5.2-beta1-aarch64-apple-darwin.tar.gz"
+      sha256 "a9298bb522ae525d873774cdbaeb9ac2d170466c645ce998e628c1880f094145"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/stevengonsalvez/agents-in-a-box/releases/download/v0.5.0/agents-box-0.5.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "f42017196d9cbd35b2143129ca2f78d2b3afa9542b7bafcf2a2852d67496a013"
+      url "https://github.com/stevengonsalvez/agents-in-a-box/releases/download/v0.5.2-beta1/agents-box-0.5.2-beta1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "cd85cc6c5c942e6b343189c69098918f51d3ef5ae42792f451742b8e5de89ef8"
     end
   end
 
   def install
     bin.install "agents-box"
+    bin.install_symlink "agents-box" => "ainb"
   end
 
   test do
