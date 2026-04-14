60e8cfde0493d746c5b8281f955974d7a1f31872
Steven Gonsalvez <steven.gonsalvez@gmail.com>
2026-04-14 16:32:51 +0100

    chore: bump ainb formula to 0.5.4-beta1

--- Formula/ainb.rb
+++ Formula/ainb.rb
@@ -2,19 +2,19 @@ class Ainb < Formula
   desc "Terminal-based development environment manager for Claude Code agents"
   homepage "https://github.com/stevengonsalvez/agents-in-a-box"
   license "MIT"
-  version "0.5.2-beta1"
+  version "0.5.4-beta1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/stevengonsalvez/agents-in-a-box/releases/download/v0.5.2-beta1/agents-box-0.5.2-beta1-aarch64-apple-darwin.tar.gz"
-      sha256 "a9298bb522ae525d873774cdbaeb9ac2d170466c645ce998e628c1880f094145"
+      url "https://github.com/stevengonsalvez/agents-in-a-box/releases/download/v0.5.4-beta1/agents-box-0.5.4-beta1-aarch64-apple-darwin.tar.gz"
+      sha256 "30abbfdf39309e91a0114ad6582eba22c7146748e195b94833fec1e779071d97"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/stevengonsalvez/agents-in-a-box/releases/download/v0.5.2-beta1/agents-box-0.5.2-beta1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "cd85cc6c5c942e6b343189c69098918f51d3ef5ae42792f451742b8e5de89ef8"
+      url "https://github.com/stevengonsalvez/agents-in-a-box/releases/download/v0.5.4-beta1/agents-box-0.5.4-beta1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "19a3dfa899396436a97d311255ecc2c3bd2b410cbc09fa5460ff9f152cbb5c86"
     end
   end
 
